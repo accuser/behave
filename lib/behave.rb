ce9d9ae875bb0e02685f373dd34b8ffb2cb5133e
Matthew Gibbons <mhgibbons@me.com>
2010-04-06 15:29:45 +0100

    Added reportable

---
+++ lib/behave.rb
@@ -0,0 +1,38 @@
+#--
+# Copyright (c) 2010 Matthew Gibbons <mhgibbons@me.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#++
+
+require 'rubygems'
+
+gem 'activemodel', '>= 3.0.0.beta2'
+gem 'activesupport', '>= 3.0.0.beta2'
+gem 'mongoid', '>= 2.0.0.beta2'
+
+require 'behave/reportable'
+
+module Behave
+  extend ActiveSupport::Concern
+
+  included do
+    include Behave::Behaviors
+  end
+end
---
+++ lib/behave/reportable.rb
@@ -0,0 +1,67 @@
+require 'active_model'
+require 'active_support'
+require 'mongoid'
+
+module Behave
+  module Behaviors
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      def reportable(options = {})
+        include Behave::Reportable unless reportable?
+
+        if options.has_key? :before
+          before_report options[:before]
+        end
+        
+        if options.has_key? :after
+          after_report options[:after]
+        end
+      end
+    
+      private
+        def reportable?
+          false
+        end
+    end
+  end
+  
+  module Reportable
+    extend ActiveSupport::Concern
+
+    included do
+      field :reported, :type => Boolean, :default => false
+      field :reported_at, :type => Time
+      field :reported_by, :type => CachedDocument
+    
+      define_model_callbacks :report
+    end
+
+    module ClassMethods
+      def reported
+        criteria.where :reported => true
+      end
+
+      def reported_by(reporter)
+        reported.where 'reported_by._type' => reporter._type, 'reporter_by._id' => reporter._id
+      end
+      
+      private
+        def reportable?
+          true
+        end
+    end
+
+    module InstanceMethods
+      def report(reporter)
+        unless reported?
+          _run_report_callbacks do
+            update_attributes :reported => true, :reported_at => Time.now, :reported_by => reporter
+          end
+        end
+
+        reported?
+      end
+    end
+  end
+end
